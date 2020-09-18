e9f39a9eb12d44be69313224d0c83262408e9954
Glisse Lisbeth Jorge Malca <glisselisbeth@MacBook-Pro-de-Glisse.local>
2020-09-18 06:59:21 -0500

    add publish heroku

--- src/main/java/com/demo/DemoApiApplication.java
+++ src/main/java/com/demo/DemoApiApplication.java
@@ -3,7 +3,10 @@ package com.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.keyholesoftware.publish.swagger.PublishSwagger;
+
 @SpringBootApplication
+@PublishSwagger
 public class DemoApiApplication {
 
 	public static void main(String[] args) {
